0447f1d6461997fd6297b7f91020b79423aa946f
Mauricio Coelho <maudrico9@gmail.com>
2023-01-13 16:04:03 -0300

    update home

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,28 +1,17 @@
-import Head from 'next/head'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import Layout from '../components/Layout'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Desafio Cuco</title>
-        <meta name="description"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Olá esse é o desafio de Desenvolvedor Full Stack do candidato Mauricio Rodrigues Coelho.
-          </p>
-        </div>
-
-        <div className={styles.center}>
-          <Link href="/clientes" className={styles.card}>
-            <h2 className={inter.className}>
+    <>  
+      <Layout>
+        <div>
+          <Link href="/clientes">
+            <h2>
               <p>
                 Acessar Clientes
               </p>              
@@ -30,9 +19,7 @@ export default function Home() {
           </Link>
           
         </div>
-
-        <div className={styles.grid}></div>
-      </main>
+      </Layout>       
     </>
   )
 }
