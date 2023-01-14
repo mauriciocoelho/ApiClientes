537fbc8a60976809d80f4f3f178e55202bd93458
Mauricio Coelho <maudrico9@gmail.com>
2023-01-14 00:03:55 -0300

    udpate clientRequest

--- backend/app/Http/Requests/ClientRequest.php
+++ backend/app/Http/Requests/ClientRequest.php
@@ -24,10 +24,25 @@ class ClientRequest extends FormRequest
     public function rules()
     {
         return [
-            'nome' => 'required|string',
-            'cpf' => 'required|cpf',
+            'nome' => 'required|string|min:3|max:255',
+            'cpf' => 'required|cpf|unique:clients',
             'data_nasc' => 'required|date',
             'telefone' => 'required|celular_com_ddd',
+        ];
+    }
+
+     public function messages()
+    {
+        return [
+            'nome.required'             => 'por favor insira o nome',
+            'nome.min'                  => 'o mínimo de caracteres é 3',
+            'cpf.required'              => 'por favor insira o CPF',
+            'cpf.cpf'                   => 'CPF invalido',
+            'cpf.unique'                => 'o CPF já consta na nossa base de dados',
+            'data_nasc.required'        => 'por favor insira a data de nascimento',
+            'data_nasc.date'            => 'data de nascimento invalida',
+            'telefone.required'         => 'por favor insira o telefone',
+            'telefone.celular_com_dd'   => 'telefone inválido',
         ];
     }
 }
